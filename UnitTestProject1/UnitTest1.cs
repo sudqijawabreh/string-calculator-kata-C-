f43e2ba6a9d5753ad3c1c25b7b7e76d2e887bc62
sudqi <sudqiomar@gmail.com>
2018-01-18 20:16:07 +0200

    Add project files.

---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using stringCalculator;
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+    }
+}
